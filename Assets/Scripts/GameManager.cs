17c8eea4d5c62e463ab70333622711a18ba39d54
Charles Carper <ccarper03@gmail.com>
2016-10-11 20:17:16 -0400

    Added a GameManager, Tim added a GameplayScript, made an enemy prefab

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+// Game Manager Class - Singleton Object
+public class GameManager : MonoBehaviour
+{
+    //C# Property to get access to singleton incstance
+    // Read only - only has get accessor
+    public static GameManager Instance
+    {
+        // Return reference to private instance
+        get
+        {
+            return Instance;
+        }
+
+    }
+
+    //-----------------------------------------------
+    private static GameManager instance = null;
+    //-----------------------------------------------
+    //HighScore
+    public int highScore = 0;
+    // Is game paused
+    public bool isPaused = false;
+    //Is player input allowed
+    public bool inputAllowed = true;
+    //Use this for initialization before Start()
+    void Awake()
+    {
+        //Check if there is an existing instance of the class in the scene
+        // if so, destroy this instance
+        if (instance)
+        {
+            DestroyImmediate(gameObject);
+            return;
+        }
+        // Make this active and only instance
+        instance = this;
+
+        // Make game manager persistent
+        DontDestroyOnLoad(gameObject);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            SceneManager.LoadScene("MainMenu");
+            Debug.Log("num one is pressed");
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            SceneManager.LoadScene("InGame");
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            SceneManager.LoadScene("Lose");
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            SceneManager.LoadScene("Win");
+        }
+        /*if (Input.GetKeyDown("keypad"))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }*/
+    }
+}
+
+
+ 
---
+++ Assets/Scripts/Gameplay.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gameplay : MonoBehaviour {
+
+	//the following are variables to store the multiple accessories for the enemy character to be randomly generated
+	public GameObject[] body;
+	public GameObject[] head;
+	public GameObject[] shirt;
+	public GameObject[] pants; //shirt and pants may be one variable or part of the body
+	public GameObject[] accessory1;
+	//miscellaneous accessories for the enemy may be optional
+
+	//timer(s) to keep track of high noon
+	float lengthOfTime;
+	float timer;
+
+	//keep track of levels
+	public int level = 1;
+
+	//boolean(s) to check and keep track of events
+	bool cursorInBox;
+
+	// Use this for initialization
+	void Start () {
+		//need to know what the period of time should be
+		lengthOfTime = Random.Range (3f, 12f);
+		timer = lengthOfTime;
+
+		//instantiate the enemy
+		//?check to instantiate either randomly generated character or specific character i.e. first and boss enemies??
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if statement to check if the cursor is NOT in the box
+		//{
+			//if timer >= 0
+			//{
+				//prompt player to put the cursor in the box
+				//timer = lengthOfTime;		//timer is reset to the original set time
+			//}
+		//}
+		//while cursor is in the box
+		//{
+			//timer -= Time.deltaTime;
+			//if(timer <= 0)
+			//{
+				//prompt user to fire weapon
+			//}
+		//}
+	}
+}
